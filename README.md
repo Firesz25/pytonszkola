5d3893335efe11feabc7e8a32bbf7fc279be6b4b
firesz25 <olek.cz@outlook.com>
2021-02-18 14:44:26 +0100

    Update script windows python instaletor

--- README.md
+++ README.md
@@ -5,13 +5,10 @@ rozwiazania do zadan z szkoly
 ## rozwiązane zadania
 
  - [x] zadanie z dnia 11.02.2021
-   - [ ] zadanie na lekcji
    - [x] zadanie domowe
  - [ ] zadanie z dnia 18.02.2021
-   - [ ] zadanie na lekcji
    - [ ] zadanie domowe
  - [ ] zadanie z dnia 25.02.2021
-   - [ ] zadanie na lekcji
    - [ ] zadanie domowe
 
 ## nazwy komitów odpowiadją dniu wrzucenia albo nazwie zadania
--- install.cmd
+++ install.cmd
@@ -1,8 +1,6 @@
-if ! [python]
-echo "python jest zainstalowany"
-else
+test python
     cd %USERPROFILE%\downloads
     curl -O https://www.python.org/ftp/python/3.8.7/python-3.8.7-amd64.exe
     echo "python został pobrany w wersji 3.8.7-amd64"
     python-3.8.7-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-    echo "python został zainstalowany"  
+    echo "python zostal zainstalowany"  
